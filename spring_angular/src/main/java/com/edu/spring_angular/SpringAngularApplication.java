03891f42e1e15a89e454f5d6c3252ea96dcc454b
mireia <mire12car34@gmail.com>
2020-04-23 21:10:36 -0500

    primer actualización
    primer commit, creacion de spring

---
+++ spring_angular/src/main/java/com/edu/spring_angular/SpringAngularApplication.java
@@ -0,0 +1,14 @@
+package com.edu.spring_angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularApplication.class, args);
+		System.out.println("hola mundo");
+	}
+
+}
